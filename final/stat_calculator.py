11a1ea756ec1538c1cfc45040c24a784a70182ef
Ryan Whitmer <rywh3896@colorado.edu>
2019-04-25 20:44:59 -0600

    Update stat_calculator.py

--- final/stat_calculator.py
+++ final/stat_calculator.py
@@ -1,5 +1,9 @@
 from pybaseball.lahman import *
+from pybaseball import statcast
 import pandas as pd
+import numpy as np
+import math
+import csv
 
 ##################################################################
 # get_teams()
@@ -46,7 +50,10 @@ def get_teams():
 
 def team_name_lookup(teamID):
 	teams = get_teams()
-	return teams[teamID]
+	if(teamID in teams):
+		return teams[teamID]
+	else:
+		return 'None'
 
 #####################################################################
 # calculate_leverage_eff_stat(teamID)
@@ -66,7 +73,83 @@ def calculate_leverage_eff_stat(teamID):
 	levEffDict = {}
 
 	# Do the calculations, store in levEffDict
+	#print(team_name_lookup(teamID))
+	statcastData = create_statcast_CSV('2017')
+	statcastData = statcastData.loc[((statcastData['home_team'] == teamID) & (statcastData['inning_topbot'] == 'Bot')) | ((statcastData['away_team'] == teamID) & (statcastData['inning_topbot'] == 'Top')), ["batter", "inning", "on_1b", "on_2b", "on_3b", "bat_score", "fld_score", "outs_when_up", "post_bat_score", "post_fld_score", "des"]]
+
+	for i in range (len(statcastData)):
+	
+		leverageGained = 0
+		leveragePossible = 0
+
+		prePlay = dict()
+		postPlay = dict()
+
+		prePlay['inning'] = statcastData.iloc[i]['inning']
+		prePlay['1b'] = statcastData.iloc[i]['on_1b']
+		prePlay['2b'] = statcastData.iloc[i]['on_2b']
+		prePlay['3b'] = statcastData.iloc[i]['on_3b']
+		prePlay['outs'] = statcastData.iloc[i]['outs_when_up']
+		prePlay['playerScore'] = statcastData.iloc[i]['bat_score']
+		prePlay['opposingScore'] = statcastData.iloc[i]['fld_score']
+		if(not (i==0)):
+			if(not pd.isnull(statcastData.iloc[i]['des'])):
+				if((not (statcastData.iloc[i]['inning'] == statcastData.iloc[i-1]['inning'])) & (not (statcastData.iloc[i]['inning'] == 9))):
+
+					postPlay['inning'] = prePlay['inning'] + 1
+					postPlay['1b'] = np.nan
+					postPlay['2b'] = np.nan
+					postPlay['3b'] = np.nan
+					postPlay['outs'] = 0
+					postPlay['playerScore'] = prePlay['playerScore']
+					postPlay['opposingScore'] = prePlay['opposingScore']
+
+				else:
+
+					postPlay['inning'] = prePlay['inning']
+					postPlay['1b'] = statcastData.iloc[i-1]['on_1b']
+					postPlay['2b'] = statcastData.iloc[i-1]['on_2b']
+					postPlay['3b'] = statcastData.iloc[i-1]['on_3b']
+					postPlay['outs'] = statcastData.iloc[i-1]['outs_when_up']
+					postPlay['playerScore'] = statcastData.iloc[i-1]['bat_score']
+					postPlay['opposingScore'] = statcastData.iloc[i-1]['fld_score']
+
+					#prePlay is the state of the game before the at-bat
+					#postPlay is the state of the game after
+					#This is where we compare the leverage change in game state and update it below
+
+				if(statcastData.iloc[i]['batter'] in levEffDict):
+					levEffDict[statcastData.iloc[i]['batter']] = (levEffDict[statcastData.iloc[i]['batter']][0] + leverageGained, levEffDict[statcastData.iloc[i]['batter']][1] + leveragePossible, levEffDict[statcastData.iloc[i]['batter']][2] + 1)
+				else:		
+					levEffDict[statcastData.iloc[i]['batter']] = (leverageGained, leveragePossible, 1)
+
+			else:
+				print(statcastData.iloc[i]['des'])
+
+	#setting up levEffDict to be key = batter (mlb player id)
+	#value = (total leverage attempted, total leverage gained, total AB)
+	#the total leverage attempted is possible leverage this player could have converted if everything went perfectly
+	#total leverage gained is what actually happened
+	#total AB is their at bats
+	#We can then convert to player first and last names with lahman's, and do whatever averaging we wanna do with the value
 
 
 	# Return the final list
 	return levEffDict
+
+
+def create_statcast_CSV(yearID):
+
+	cwd = os.getcwd()
+	if os.path.exists(cwd + "/home/local_data/statcast" + yearID + '.csv'):
+		file = pd.read_csv(cwd + "/home/local_data/statcast" + yearID + '.csv')
+	else:
+		file = statcast(start_dt=yearID + '-04-01', end_dt=yearID + '-11-01')
+		pd.DataFrame.to_csv(file, cwd + "/home/local_data/statcast" + yearID + '.csv', ',')
+
+	return file
+
+
+print(calculate_leverage_eff_stat('SEA'))
+
+
